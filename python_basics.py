eb2e3b887e85f39be4e0c02c449b66891cb2a6c0
Lukas <lukaskostiugovas@gmail.com>
2025-05-29 19:28:39 +0300

    Added some variables

--- python_basics.py
+++ python_basics.py
@@ -2,3 +2,20 @@ print("Hello, World!");
 print("Goodbye, World!");
 print("Welcome!")
 print("Looks like this works!")
+
+name = "Lukas" # String variable
+age = 29 # Integer variable
+height = 1.81 # Float variable
+is_married = True # Boolean variable
+is_turtle = False # Boolean variable
+
+
+is_old_enough = age >= 18 # Boolean expression
+
+
+
+print(f"My name is {name} and I am {age} years old.")
+print(f"My height is {height} meters.")
+print(f"Am I married? {'Yes' if is_married else 'No'}")
+print(f"Am I a turtle? {'Yes' if is_turtle else 'No'}")
+print(f"Am I old enough? {'I guess so, yes' if is_old_enough else 'No'}")
