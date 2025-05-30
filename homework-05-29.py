dc8f9deaaefe4389d8c036651deeb8e649b236ac
Lukas <lukaskostiugovas@gmail.com>
2025-05-30 08:57:20 +0300

    Final version of homework for 05-29

--- homework-05-29.py
+++ homework-05-29.py
@@ -1,22 +1,25 @@
-user_bd_year = int(input("Enter your birth year: "))  # Input from user as an integer
-user_bd_month = int(input("Enter your birth month: "))  # Input from user as an integer
-user_bd_day = int(input("Enter your birth day: "))  # Input from user as an integer
+birth_year = int(input("Enter your birth year: "))  # Input from user as an integer
+birth_month = int(input("Enter your birth month: "))  # Input from user as an integer
+birth_day = int(input("Enter your birth day: "))  # Input from user as an integer
 
+# Automatically calculate the current date
+from datetime import date # Getting the current date; importing date from datetime module
+current_date = date.today() # Date today
 
+# Calculating age
+age = current_date.year - birth_year # Age is the difference between current year and birth year
+if current_date.month < birth_month or (current_date.month == birth_month and current_date.day < birth_day): # Check if the birthday has occurred this year
+    age -= 1  # -1 year, if the birthday hasn't occurred yet this year
 
-from datetime import datetime 
-from dateutil.relativedelta import relativedelta
+print() # Blank line for better readability
+print(f"You are {age} years old.")
+print() # Blank line for better readability
 
-birth_date = datetime(user_bd_year, user_bd_month, user_bd_day) # Create a datetime object for the birth date
-
-today = datetime.now()
-
-
-difference = relativedelta(today, birth_date)
-
-
-# Get age in years
-age_in_years = difference.years
-
-print()# Print the user's age in years, months, and days
-print(f"You are {difference.years} years old.")
+# Creating categories based on age
+if age <= 17 and age >= 0: # Age is between 0 and 17
+    print("Age category: I.")
+elif age <= 36 and age >= 18: # Age is between 18 and 36
+    print("Age category: II.")
+else :   # Age is 37 or older
+    print("Age category: III.") 
+print() # Blank line for better readability
