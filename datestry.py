9733ef57db672cc41bc09429b5b3aa8deaba0306
Lukas <lukaskostiugovas@gmail.com>
2025-06-03 19:06:40 +0300

    lesson on 06/03

---
+++ datestry.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+print("Current date and time:", now)
+print("Current year:", now.year)
+print("Current month:", now.month)
+print("Current day:", now.day)
+print("Current hour:", now.hour)
+print("Current minute:", now.minute)
+print("Current second:", now.second)
